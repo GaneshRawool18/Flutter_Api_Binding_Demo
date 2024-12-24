0d47b96f40112451b1766fb15e14eb6868fc7c47
GaneshRawool18 <ganeshrawool07@gmail.com>
2024-12-24 23:10:44 +0530

    Updating main.dart file for hanging navigation

--- api_binding/lib/main.dart
+++ api_binding/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:api_binding/home_Page.dart';
+import 'package:api_binding/get_started_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: GetStartedPage(),
     );
   }
 }
